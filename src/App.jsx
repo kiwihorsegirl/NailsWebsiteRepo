9a1768c97083ce32d2dd35d364e2b1087ad8bdde
Elisa K <elisa.krippner@gmail.com>
2025-06-06 16:40:36 +1200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vanessa's Nails</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Text
         </p>
       </div>
       <p className="read-the-docs">
